bfe0640b80aad927455f098936bf23c75c18c120
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-11 23:40:05 +0530

    swagger_openapi for documentation

---
+++ swagger_openApi_v3.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OpenApi;
+
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="Canonizer API",
+ *         description="This documentation is the canonizer version 3 API",
+ *         termsOfService="http://canonizer.com/termsandservice",
+ *         @OA\Contact(
+ *             email="support@canonizer.com"
+ *         )
+ *     ),
+ *     @OA\Server(
+ *         description="Canonizer API",
+ *         url="https://api.canonizer.com/api/v3/"
+ *     )
+ * )
+ */
+class OpenApiSpec
+{
+}
