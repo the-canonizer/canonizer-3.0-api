f06c3ec3096b8b0518920170ede6d1131e12d40e
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-02-18 16:38:26 +0530

    Update typos in message config

--- config/message.php
+++ config/message.php
@@ -8,16 +8,12 @@ return [
         'email_invalid'  => 'Invalid Email Id!'
     ],
     'success' => [
-        'success'         => 'Success',
-        'password_change' => 'Password changed successfully.',
-        'update_profile'  => 'Profile updated successfully.',
-        'verify_otp'      => 'Phone number has been verified successfully.',
-<<<<<<< HEAD
-        'forgot_password' => 'Otp sent successfully on your  Email Id.'
-=======
+        'success'          => 'Success',
+        'password_change'  => 'Password changed successfully.',
+        'update_profile'   => 'Profile updated successfully.',
+        'verify_otp'       => 'Phone number has been verified successfully.',
+        'forgot_password'  => 'Otp sent successfully on your Email Id.',
         'nick_name_update' => 'Nick name visibility status updated successfully.',
-        'nick_name_add' => 'Nick name added successfully.'
->>>>>>> origin/development
+        'nick_name_add'    => 'Nick name added successfully.'
     ]
-
 ];
