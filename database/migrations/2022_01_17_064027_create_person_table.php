ec4763a5bb8195c891224ad9a2560553b957442e
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-03-17 17:50:07 +0530

    Changes in migrations

--- database/migrations/2022_01_17_064027_create_person_table.php
+++ database/migrations/2022_01_17_064027_create_person_table.php
@@ -13,34 +13,36 @@ class CreatePersonTable extends Migration
      */
     public function up()
     {
-        Schema::create('person', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name', 200);
-            $table->string('middle_name', 200)->nullable();
-            $table->string('last_name', 200)->nullable();
-            $table->string('email', 200)->nullable();
-            $table->string('password', 255)->nullable();
-            $table->string('address_1', 255)->nullable();
-            $table->string('address_2', 255)->nullable();
-            $table->string('city', 255)->nullable();
-            $table->string('state', 255)->nullable();
-            $table->string('postal_code', 255)->nullable();
-            $table->string('country', 255)->nullable();
-            $table->string('phone_number', 15)->nullable();
-            $table->string('mobile_carrier', 100)->nullable();
-            $table->tinyInteger('mobile_verified')->default(0)->comment('0 => Not Verified, 1 => Verified');
-            $table->integer('update_time')->nullable();
-            $table->integer('join_time')->nullable();
-            $table->string('language', 255)->nullable();
-            $table->string('birthday', 255)->nullable();
-            $table->tinyInteger('gender')->nullable();
-            $table->set('private_flags',['first_name','middle_name','last_name','email','birthday','address_1','address_2','city','state','postal_code','country'])->nullable();
-            $table->string('remember_token',255)->nullable();
-            $table->string('default_algo',255)->default('blind_popularity');
-            $table->string('type',255)->default('user');
-            $table->string('otp',255)->nullable();
-            $table->tinyInteger('status')->default(0);
-        });
+        if (!Schema::hasTable('person')) {
+            Schema::create('person', function (Blueprint $table) {
+                $table->id();
+                $table->string('first_name', 200);
+                $table->string('middle_name', 200)->nullable();
+                $table->string('last_name', 200)->nullable();
+                $table->string('email', 200)->nullable();
+                $table->string('password', 255)->nullable();
+                $table->string('address_1', 255)->nullable();
+                $table->string('address_2', 255)->nullable();
+                $table->string('city', 255)->nullable();
+                $table->string('state', 255)->nullable();
+                $table->string('postal_code', 255)->nullable();
+                $table->string('country', 255)->nullable();
+                $table->string('phone_number', 15)->nullable();
+                $table->string('mobile_carrier', 100)->nullable();
+                $table->tinyInteger('mobile_verified')->default(0)->comment('0 => Not Verified, 1 => Verified');
+                $table->integer('update_time')->nullable();
+                $table->integer('join_time')->nullable();
+                $table->string('language', 255)->nullable();
+                $table->string('birthday', 255)->nullable();
+                $table->tinyInteger('gender')->nullable();
+                $table->set('private_flags', ['first_name', 'middle_name', 'last_name', 'email', 'birthday', 'address_1', 'address_2', 'city', 'state', 'postal_code', 'country'])->nullable();
+                $table->string('remember_token', 255)->nullable();
+                $table->string('default_algo', 255)->default('blind_popularity');
+                $table->string('type', 255)->default('user');
+                $table->string('otp', 255)->nullable();
+                $table->tinyInteger('status')->default(0);
+            });
+        }
     }
 
     /**
--- database/migrations/2022_02_17_104704_create_ads_table.php
+++ database/migrations/2022_02_17_104704_create_ads_table.php
@@ -13,22 +13,24 @@ class CreateAdsTable extends Migration
      */
     public function up()
     {
-        Schema::create('ads', function (Blueprint $table) {
-            $table->id();
-            $table->string('client_id', 50);
-            $table->unsignedBigInteger('page_id');
-            $table->string('slot', 50);
-            $table->string('format', '50')->nullable();
-            $table->tinyInteger('adtest')->default('0')->comment('0 => Off, 1 => On'); 
-            $table->tinyInteger('is_responsive')->default(1)->comment('0 => True, 1 => Off');
-            $table->tinyInteger('status')->default(1)->comment('0 => Inactive, 1 => Active');
-            $table->integer('create_time');
-            $table->integer('update_time');
-        });
+        if (!Schema::hasTable('ads')) {
+            Schema::create('ads', function (Blueprint $table) {
+                $table->id();
+                $table->string('client_id', 50);
+                $table->unsignedBigInteger('page_id');
+                $table->string('slot', 50);
+                $table->string('format', '50')->nullable();
+                $table->tinyInteger('adtest')->default('0')->comment('0 => Off, 1 => On');
+                $table->tinyInteger('is_responsive')->default(1)->comment('0 => True, 1 => Off');
+                $table->tinyInteger('status')->default(1)->comment('0 => Inactive, 1 => Active');
+                $table->integer('create_time');
+                $table->integer('update_time');
+            });
 
-        Schema::table('ads', function($table) {
-            $table->foreign('page_id')->references('id')->on('pages');
-        });         
+            Schema::table('ads', function ($table) {
+                $table->foreign('page_id')->references('id')->on('pages');
+            });
+        }
     }
 
     /**
