135026168d1714efc0d7ced619007452ba201435
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-04-11 16:46:32 +0530

    create unit test case for create thread API

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class Thread extends Model
+
+class Thread extends Model implements AuthenticatableContract, AuthorizableContract
 {
+    use Authenticatable, HasApiTokens  , Authorizable, HasFactory;
+    
     protected $table = 'thread';
     public $timestamps = false;
 
---
+++ tests/ThreadsApiTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use App\Models\Camp;
+use App\Models\Thread;
+use Laravel\Lumen\Testing\WithoutMiddleware;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ThreadsApiTest extends TestCase
+{
+
+    use WithoutMiddleware;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testThreadStoreValidateFiled()
+    {
+        $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+
+        $rules = [
+            'title'    => 'required|max:100|regex:/^[a-zA-Z0-9\s]+$/',
+            'nick_name' => 'required',
+            'camp_num' => 'required',
+            'topic_num' => 'required',
+            'topic_name' => 'required',
+        ];
+
+        $data = [
+            "title" => "Test 3",
+            "nick_name" => "449",
+            "camp_num" => "1",
+            "topic_num" => "290",
+            "topic_name" => "Saurabh singh te11s111t 142"
+        ];
+
+        $v = $this->app['validator']->make($data, $rules);
+        $this->assertTrue($v->passes());
+    }
+
+    public function testThreadStoreWithInvalidData()
+    {
+        print sprintf(" \n Invalid Thread Store details submitted %d %s", 400, PHP_EOL);
+
+        $Thread = Thread::factory()->make();
+        $parameter = [
+            "title" => "",
+            "nick_name" => "",
+            "camp_num" => "",
+            "topic_num" => "",
+            "topic_name" => ""
+        ];
+
+        $this->actingAs($Thread)->post('/api/v3/thread/save', $parameter);
+        $this->assertEquals(400, $this->response->status());
+    }
+
+    public function testThreadStoreWithValidData()
+    {
+        print sprintf(" \n Valid Thread Store details submitted %d %s", 200, PHP_EOL);
+
+        $rand = rand(10, 99);
+        $parameters = [
+            "title" => "Test 3",
+            "nick_name" => "449",
+            "camp_num" => "1",
+            "topic_num" => "290",
+            "topic_name" => "Saurabh singh te11s111t 142"
+        ];
+        $this->call('POST', '/api/v3/thread/save', $parameters);
+        $this->seeJsonStructure([
+            'status_code',
+            'message',
+            'error',
+            'data' => []
+        ]);
+    }
+}
