13038d3b32c8deb341acc282ba7f9c65abdb9c35
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-06-14 14:27:20 +0530

    change grace period time camp and topic

--- app/Http/Controllers/CampController.php
+++ app/Http/Controllers/CampController.php
@@ -178,7 +178,7 @@ class CampController extends Controller
                 "camp_about_url" => $request->camp_about_url ?? "",
                 "title" => $request->title ?? "",
                 "camp_about_nick_id" =>  $request->camp_about_nick_id,
-                "grace_period" => 1
+                "grace_period" => 0
             ];
 
             $camp = Camp::create($input);
