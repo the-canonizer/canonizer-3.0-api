dc88ea936db31e2067d4960c973f56d6ba49388a
M-Darab <m.darabkhan@codedistrict.com>
2022-06-16 19:46:05 +0500

    social media icons link fix

--- database/seeders/SocialMediaLinksSeeder.php
+++ database/seeders/SocialMediaLinksSeeder.php
@@ -18,31 +18,31 @@ class SocialMediaLinksSeeder extends Seeder
             array(
                 'label' => 'Facebook',
                 'link' => 'https://www.facebook.com/pages/Canonizer.com/103927141540408/',
-                'icon' => '/images/social-media/facebook.svg',
+                'icon' => '/social-media/facebook.svg',
                 'order_number' => 1,
             ),
             array(
                 'label' => 'Instagram',
                 'link' => 'https://www.instagram.com/',
-                'icon' => '/images/social-media/instagram.svg',
+                'icon' => '/social-media/instagram.svg',
                 'order_number' => 2,
             ),
             array(
                 'label' => 'Twitter',
                 'link' => 'https://www.twitter.com/',
-                'icon' => '/images/social-media/twitter.svg',
+                'icon' => '/social-media/twitter.svg',
                 'order_number' => 3,
             ),
             array(
                 'label' => 'Youtube',
                 'link' => 'https://www.youtube.com/',
-                'icon' => '/images/social-media/youtube.svg',
+                'icon' => '/social-media/youtube.svg',
                 'order_number' => 4,
             ),
             array(
                 'label' => 'LinkedIn',
                 'link' => 'https://www.linkedin.com/',
-                'icon' => '/images/social-media/linkedIn.svg',
+                'icon' => '/social-media/linkedIn.svg',
                 'order_number' => 5,
             ),
         );
