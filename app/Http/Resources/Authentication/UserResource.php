f52bddc734856c8ce4fa31f09a41db668f3094a4
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-09-05 17:52:31 +0530

    add user id in auth response

--- app/Http/Resources/Authentication/UserResource.php
+++ app/Http/Resources/Authentication/UserResource.php
@@ -9,6 +9,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id"      => $this->id ?? '',
             "first_name"      => $this->first_name ?? '',
             "middle_name"     => $this->middle_name ?? '',
             "last_name"       => $this->last_name ?? '',
