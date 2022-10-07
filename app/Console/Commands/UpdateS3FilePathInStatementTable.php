9ff2894a5873f28c0cf255e4cddfb4e87ac07f50
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-10-07 14:15:56 +0530

    changes

--- app/Console/Commands/UpdateS3FilePathInStatementTable.php
+++ app/Console/Commands/UpdateS3FilePathInStatementTable.php
@@ -40,16 +40,16 @@ class UpdateS3FilePathInStatementTable extends Command
      */
     public function handle()
     {
-        $statementRecords = Statement::where('value', 'like', '%https://canonizer.com/files/%')
-            ->orwhere('value', 'like', '%http://canonizer.com/files/%')
+        $statementRecords = Statement::where('value', 'like', '%https://canonizer.com/files%')
+            ->orwhere('value', 'like', '%http://canonizer.com/files%')
             ->get();
         $updateRecored = 0;
         if ($statementRecords) {
             foreach ($statementRecords as $val) {
-                $val->value = Str::replace('http://canonizer.com/files/', env('AWS_URL'), $val->value);
-                $val->value = Str::replace('https://canonizer.com/files/', env('AWS_URL'), $val->value);
-                $val->note = Str::replace('http://canonizer.com/files/', env('AWS_URL'), $val->note);
-                $val->note = Str::replace('https://canonizer.com/files/', env('AWS_URL'), $val->note);
+                $val->value = Str::replace('http://canonizer.com/files', env('AWS_URL'), $val->value);
+                $val->value = Str::replace('https://canonizer.com/files', env('AWS_URL'), $val->value);
+                $val->note = Str::replace('http://canonizer.com/files', env('AWS_URL'), $val->note);
+                $val->note = Str::replace('https://canonizer.com/files', env('AWS_URL'), $val->note);
                 $val->update();
                 $updateRecored++;
             }
