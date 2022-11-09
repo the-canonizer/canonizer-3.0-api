50a164321d24b4f470d70a0243464d00b8c1ea12
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-11-09 15:41:20 +0530

    changes in test case

--- resources/lang/en/testSample.php
+++ resources/lang/en/testSample.php
@@ -6,7 +6,18 @@ return [
             'admin_1' => '1'
         ],
         'normal_user' => [
-            'user_1' => '362'
+            'user_1' => '362',
+            'user_2' => [
+                'id' => '1252',
+                'email' => 'saurabh.singh@iffort.com',
+                'password' => 'Test@123',
+                "first_name" => "saurabh",
+                "last_name" => "singh",
+                "middle_name" => "kumar",
+                "phone_number" => "8765432123",
+                "country_code" => "+91",
+                "otp" => "123456",
+            ]
         ]
     ],
 ];
