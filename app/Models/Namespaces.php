08be2a69cefb9401194748cb3cf6010e921a0a22
Kshitij Singh <kshitij.singh@iffort.com>
2022-11-11 12:00:35 +0530

    CAN-1450 Fixes namespace label issue

--- app/Models/Namespaces.php
+++ app/Models/Namespaces.php
@@ -10,4 +10,21 @@ class Namespaces extends Model
     public $timestamps = false;
 
     protected $guarded = [];
+
+    public static function getNamespaceLabel($namespace,$label = ''){
+        if(isset($label[0]) && $label[0] !='/'){
+            $label = "/".$label;
+        }
+   
+       if((!empty($label) && $label[strlen($label) - 1] != '/')){
+           $label = $label."/";
+       }
+   
+        if($namespace->parent_id != 0 && $namespace->id != $namespace->parent_id){
+            $nameSpaceParent = self::find($namespace->parent_id);
+            return self::getNamespaceLabel($nameSpaceParent,$nameSpaceParent->name.$label);
+        }
+   
+        return $label;
+    }
 }
