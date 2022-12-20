54be287c2912fd32e5f7897c5b91908323d1ea20
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2022-12-20 14:35:45 +0530

    Fixed #103

--- app/Models/Camp.php
+++ app/Models/Camp.php
@@ -812,4 +812,23 @@ class Camp extends Model implements AuthenticatableContract, AuthorizableContrac
 
         return $camparray;
     }
+
+    public static function checkAllLiveCampsInTopic($topicnum){ 
+        return self::where('topic_num', '=', $topicnum)
+                        ->where('objector_nick_id', '=', NULL)
+                        ->whereRaw('go_live_time in (select max(go_live_time) from camp where topic_num=' . $topicnum . ' and objector_nick_id is null and go_live_time < "' . time() . '" group by camp_num)')
+                        ->where('go_live_time', '<=', time())
+                        ->groupBy('camp_num')
+                        ->orderBy('submit_time', 'desc')
+                        ->get();
+    }
+
+    public static function checkAllNonLiveCampsInTopic($topicnum){ 
+        return self::where('topic_num', '=', $topicnum)
+                        ->where('objector_nick_id', '=', NULL)
+                        ->where('go_live_time',">",time())
+                        ->groupBy('camp_num')
+                        ->orderBy('submit_time', 'desc')
+                        ->get();
+    }
 }
