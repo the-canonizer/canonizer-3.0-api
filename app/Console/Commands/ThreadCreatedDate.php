9686e687bfa9f555c35d77dfc592f111a649b39d
Kshitij Singh <kshitij.singh@iffort.com>
2023-01-17 19:11:34 +0530

    Added class documentation for ThreadCreatedDate

--- app/Console/Commands/ThreadCreatedDate.php
+++ app/Console/Commands/ThreadCreatedDate.php
@@ -7,6 +7,14 @@ use App\Models\Thread;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 
+/**
+ * This command will update the created_at & updated_at date of the Thread 
+ * if the existing creation date of the thread is greater than the creation date of the oldest first post. 
+ * Before running this command, please take the backup of the Threads table.
+ * NOTE: This command should only run when we are absolutely sure to update the created_at 
+ * and updated_at date of the thread
+ */
+
 class ThreadCreatedDate extends Command
 {
     /**
@@ -47,16 +55,16 @@ class ThreadCreatedDate extends Command
         if ($threadRecords) {
             $postRecord = '';
             foreach ($threadRecords as $threadRecord) {
-                    $postRecord = Reply::select('id', 'c_thread_id', 'created_at', 'updated_at')->where('c_thread_id', $threadRecord->id)->orderBy('created_at', 'ASC')->first();
-                    if ($postRecord) {
-                        $postCount++;
-                        if ($threadRecord->created_at > $postRecord->created_at) {
-                            $threadRecord->created_at = $postRecord->created_at;
-                            $threadRecord->updated_at = $postRecord->updated_at;
-                            $threadRecord->save();
-                            $updateRecored++;
-                        }
+                $postRecord = Reply::select('id', 'c_thread_id', 'created_at', 'updated_at')->where('c_thread_id', $threadRecord->id)->orderBy('created_at', 'ASC')->first();
+                if ($postRecord) {
+                    $postCount++;
+                    if ($threadRecord->created_at > $postRecord->created_at) {
+                        $threadRecord->created_at = $postRecord->created_at;
+                        $threadRecord->updated_at = $postRecord->updated_at;
+                        $threadRecord->save();
+                        $updateRecored++;
                     }
+                }
             }
         }
 
