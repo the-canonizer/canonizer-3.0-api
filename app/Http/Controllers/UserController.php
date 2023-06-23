cd20e9ba0580bad0a623e5e10cec67caf5fd8a53
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2023-06-23 15:15:30 +0530

    Change status code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -260,7 +260,7 @@ class UserController extends Controller
             ];
             $validateRecaptcha = Util::httpPost($postUrl, $payload);
             if ($validateRecaptcha->status_code != 200 || !$validateRecaptcha->data['success'] || $validateRecaptcha->data['score'] < 0.5) {
-                $status = 403;
+                $status = 304;
                 $message = "The reCAPTCHA verification failed, please try again.";
                 if ($validateRecaptcha->status_code != 200) {
                     $message = "An error occurred during reCAPTCHA verification.";
