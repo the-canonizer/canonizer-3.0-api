05bbd64e89f039cab3b5f755ed9756c81d3a17c2
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2023-07-25 13:15:22 +0530

    Add base url in sitemap index

--- app/Http/Controllers/SitemapXmlController.php
+++ app/Http/Controllers/SitemapXmlController.php
@@ -44,7 +44,7 @@ class SitemapXmlController extends Controller
         $lastModified = Carbon::now()->startOfDay()->toIso8601String();
         $siteMaps = array_map(function ($url) use ($lastModified) {
             return [
-                'url' => $url,
+                'url' => env('APP_URL_FRONT_END').'/'.$url,
                 'last_modified' => $lastModified,
             ];
         }, $urls);
