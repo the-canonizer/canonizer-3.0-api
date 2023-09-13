6f40732cef4d2c99721d230efb810073ffc56f0c
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2023-09-13 15:13:17 +0530

    remove email  related changes from this branch

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -8,12 +8,14 @@ use Carbon\Carbon;
 
 class Helpers
 {
-    public static function renderParentCampLinks($topic_num, $camp_num, $topic_name, $withLinks = false, $seprator = '>>')
+    public static function renderParentCampLinks($topic_num, $camp_num, $topic_name, $withLinks = false, $seprator = '>')
     {
-
-        $filter['topicNum'] = $topic_num;
-        $filter['campNum'] = $camp_num ?? 1;
-        $camp = Camp::getLiveCamp($filter);
+        $camp = Camp::where([
+            'camp_num' => $camp_num,
+            'topic_num' => $topic_num,
+            'grace_period' => 0,
+            'objector_nick_id' => null,
+        ])->orderBy('submit_time', 'desc')->first();
 
         if (!$camp) {
             return "";
@@ -21,7 +23,7 @@ class Helpers
 
         if (is_null($camp->parent_camp_num)) {
             $topicLink = Topic::topicLink($topic_num, 1, $topic_name);
-            return self::createLink($topic_name . ' ' . $seprator . ' ' . $camp->camp_name, $topicLink) ?? $camp->camp_name;
+            return self::createLink($topic_name, $topicLink) ?? $camp->camp_name;
         }
 
         $campLink = Topic::topicLink($topic_num, $camp->camp_num, $topic_name, $camp->camp_name);
