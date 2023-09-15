a60ef2d655802a4155cfefebef14eca5f20c9ef7
Reena Nalwa <reenanalwa@gmail.com>
2023-09-15 10:29:52 +0530

    added search files

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    
+    public function getSearchResults()
+    {
+          echo 'vfdv';
+    }
+
+
+    public function importDataToElasticSarch()
+    {
+        echo 'sset';
+    }
+
+}
