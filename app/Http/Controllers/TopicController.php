5eee86fe8c147cd8c2384808bcd408d8936b58d3
M-Darab <m.darabkhan@codedistrict.com>
2023-09-28 12:59:13 +0500

    CAN-API-510: Log placed to track

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -495,7 +495,8 @@ class TopicController extends Controller
             // $model->go_live_time = strtotime(date('Y-m-d H:i:s', strtotime('+1 days')));
             Log::info('<===============================>');
             Log::info('ifIamSingleSupporter => '. $ifIamSingleSupporter);
-            Log::info('archiveReviewPeriod => '. $archiveReviewPeriod);
+            Log::info('ifIamSingleSupporter => '. $ifIamSingleSupporter);
+            Log::info('$nickNames => '. $nickNames);
             Log::info('if => '. ($ifIamSingleSupporter && !$archiveReviewPeriod));
             $model->go_live_time = Carbon::now()->addSeconds(env('LIVE_TIME_DELAY_IN_SECONDS') - 10)->timestamp;
             if($ifIamSingleSupporter && !$archiveReviewPeriod) {
