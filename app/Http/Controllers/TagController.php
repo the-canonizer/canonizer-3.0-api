44100c5dfe8da708ae9f40ba25eed8a8c9e39156
Ashish Upadhyay <ashish.upadhyay@iffort.com>
2024-07-04 09:32:38 +0530

    change shorting in to name

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -105,7 +105,7 @@ class TagController extends Controller
                     $query->where(function ($q) use ($result) {
                         $q->where('title', 'LIKE', '%' . $result . '%');
                     });
-                })->orderBy('id', $request->sort_by ?? 'DESC');
+                })->orderBy('name', $request->sort_by ?? 'ASC');
 
             if($request->has('per_page') && $request->has('page')) {
                 $topic_tags = $topic_tags->paginate($perPage);
