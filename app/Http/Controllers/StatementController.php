1c375bbdb06fc6ff969f71bbedb435e277c7dd4f
array-junkie <m.darabkhan@codedistrict.com>
2025-01-08 11:54:14 +0500

    CAN-API-1612: Topic Tags added in Topic Comparison Page

--- app/Http/Controllers/StatementController.php
+++ app/Http/Controllers/StatementController.php
@@ -813,7 +813,7 @@ class StatementController extends Controller
                 $statement['latestRevision'] = ($latestRevision->submit_time);
             }
             if ($request->compare == 'topic') {
-                $campStatement =  Topic::whereIn('id', $request->ids)->get();
+                $campStatement =  Topic::whereIn('id', $request->ids)->with('topicTags.tag:id,title')->get();
 
                 foreach ($campStatement as $val) {
 
@@ -837,7 +837,9 @@ class StatementController extends Controller
                         'submitter_nick_name' => Nickname::getUserByNickId($val->submitter_nick_id),
                         'status' => $status ?? null,
                         'namespace_id' => $val->namespace_id,
-                        'namespace' => Namespaces::find($val->namespace_id)->label
+                        'namespace' => Namespaces::find($val->namespace_id)->label,
+                        'is_rank_hidden' => $val->is_rank_hidden,
+                        'topic_tags' => $val->topicTags,
                     );
                 }
                 $filter['topicNum'] = $request->topic_num;
