613858acaef0551544eae5c227899aa85c2a725d
array-junkie <m.darabkhan@codedistrict.com>
2025-01-13 17:39:56 +0500

    CAN-API-1463: Draft Record Added

--- app/Http/Controllers/StatementController.php
+++ app/Http/Controllers/StatementController.php
@@ -489,7 +489,7 @@ class StatementController extends Controller
                 }
             }
 
-            return $this->resProvider->apiJsonResponse(200, $message, (isset($all['is_draft']) && $all['is_draft'] ? [ "draft_record_id" => $statement->id] : ''), '');
+            return $this->resProvider->apiJsonResponse(200, $message, (isset($all['is_draft']) && $all['is_draft'] ? ["draft_record_id" => $statement->id, 'draft_record' => $statement] : ''), '');
         } catch (Exception $e) {
             return $this->resProvider->apiJsonResponse(400, trans('message.error.exception'), '', $e->getMessage());
         }
