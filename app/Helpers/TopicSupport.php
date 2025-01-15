f267738ce503459687e2ecb0fe1a86c4af2617e1
shavetatalentelgia <shaveta.aggarwal@talentelgia.in>
2025-01-15 14:01:24 +0530

    refactor the code

--- app/Helpers/TopicSupport.php
+++ app/Helpers/TopicSupport.php
@@ -797,15 +797,12 @@ class TopicSupport
 
     public static function groupCampsForNickId($results, $nickname,$namespaceId = 1)
     {
-        try{
-
             $supports[$nickname->id] = [];
             $supports[$nickname->id]['nick_name_id'] = $nickname->id;
             $supports[$nickname->id]['nick_name'] = $nickname->nick_name;
             $supports[$nickname->id]['private_status'] = $nickname->private;
-            if (!isset($supports[$nickname->id]['topic'])) {
-                $supports[$nickname->id]['topic'] = [];
-            }
+            $supports[$nickname->id]['topic'] = [];
+            
             foreach ($results as $rs) {
                 $topic_num = $rs->topic_num;
                 $camp_num = $rs->camp_num;
@@ -830,7 +827,6 @@ class TopicSupport
                 }
     
                 if ($rs->delegate_nick_name_id && $camp_num != 1 ) {
-    
                     $tempCamp = [
                                 'camp_name' => $livecamp->camp_name, 
                                 'camp_num' => $camp_num, 
@@ -847,7 +843,6 @@ class TopicSupport
                     }
     
                 } else if ($camp_num == 1) { 
-    
                     //$supports[$nickname->id]['topic'][$topic_num]['camp_name'] = ($rs->camp_name != "") ? $livecamp->camp_name : $livecamp->title;
                     $supports[$nickname->id]['topic'][$topic_num]['topic_num'] = $topic_num;
                     $supports[$nickname->id]['topic'][$topic_num]['title_link'] = Topic::topicLink($topic_num, 1, $title);
@@ -857,10 +852,8 @@ class TopicSupport
                     if($rs->delegate_nick_name_id){
                         $supports[$nickname->id]['topic'][$topic_num]['delegate_nick_name_id'] = $rs->delegate_nick_name_id;
                         $supports[$nickname->id]['topic'][$topic_num]['delegate_nick_name'] = $delegatedToNickname;
-                    }
-                    
+                    } 
                 } else {
-    
                     $tempCamp = [
                         'camp_name' => $livecamp->camp_name, 
                         'camp_num' => $camp_num, 
@@ -869,7 +862,6 @@ class TopicSupport
                         'delegate_nick_name_id'=>$rs->delegate_nick_name_id,
                         'delegate_nick_name' => $delegatedToNickname
                     ];
-    
                     if(isset($supports[$nickname->id]['topic'][$topic_num]['camps'])){
                         array_push($supports[$nickname->id]['topic'][$topic_num]['camps'],$tempCamp);
                     }else{
@@ -877,27 +869,19 @@ class TopicSupport
                     }
                 }
             }
-    
             $hasCamps = false;
             foreach ($supports[$nickname->id]['topic'] as $topic_data) {
                 if (!empty($topic_data['camps'])) {
                     $hasCamps = true;
-                    break;  // If any topic has camps, we set this flag to true
+                    break;
                 }
             }
-        
             // If no camps are found for the nickname, return null or empty array
             if (!$hasCamps) {
                 return []; // Or return [] if you prefer an empty array
             }
-    
             return $supports;
         }
-        catch (\Exception $e){
-            dd($e);
-        }
-     
-    }
 
     /**
      * [Recursive function ro remove delegate support]
