2e457634cfe330cc11b61ed9ec068fda511f52d5
shavetatalentelgia <shaveta.aggarwal@talentelgia.in>
2025-02-13 14:10:02 +0530

    Remove Duplicate Link

--- database/data/modified_existing_data_to_elasticsearch.sql
+++ database/data/modified_existing_data_to_elasticsearch.sql
@@ -83,7 +83,6 @@ BEGIN
             0 AS nick_name_id,
             REPLACE(TRIM(BOTH '/' FROM c.label), '/', ' > ') AS namespace,
             CONCAT('topic/',a.topic_num,'-',REPLACE(REPLACE(REPLACE(a.topic_name,'https://','https---'),'.','-'),'/','-'),'/1-Agreement') AS link,
-            CONCAT('topic/',a.topic_num,'-',REPLACE(a.topic_name,' ','-'),'/1-Agreement') AS link,
             JSON_OBJECT() AS breadcrumb_data,
             '' AS support_count,
             'topic' AS `type`
